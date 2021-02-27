75df9d205ab895ee72dc2b85eafc0fc3006f7299
sato064 <satoberu@gmail.com>
2021-02-27 23:30:07 +0900

    make/ketsuo

--- 1week2021_02/Assets/movePlayer.cs
+++ 1week2021_02/Assets/movePlayer.cs
@@ -23,7 +23,6 @@ Vector3 prevPos;
     void Update()
     {
 		
-		
         if (transform.position == target) {
 			SetTargetPosition ();
 		}
@@ -33,6 +32,7 @@ Vector3 prevPos;
     void SetTargetPosition(){
 
 		GameObject obj = (GameObject)Resources.Load("downBrock");
+		GameObject lobj = (GameObject)Resources.Load("leftBrock");
  
 		prevPos = target;
 		
@@ -64,6 +64,91 @@ Vector3 prevPos;
 			Instantiate(obj,new Vector3(1.2f,4.2f,0.0f),Quaternion.identity);
 			}
 
+			if(ran == 0){
+			Instantiate(obj,new Vector3(-1.8f,4.2f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 1){
+			Instantiate(obj,new Vector3(-2.4f,4.2f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 2){
+			Instantiate(obj,new Vector3(-3.0f,4.2f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 3){
+			Instantiate(obj,new Vector3(1.8f,4.2f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 4){
+			Instantiate(obj,new Vector3(2.4f,4.2f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 0){
+			Instantiate(lobj,new Vector3(-4.2f,1.8f,0.0f),Quaternion.identity);
+			}
+
+
+			if(ran == 1){
+			Instantiate(lobj,new Vector3(-4.2f,2.4f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 2){
+			Instantiate(lobj,new Vector3(-4.2f,3.0f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 3){
+			Instantiate(lobj,new Vector3(-4.2f,3.6f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 4){
+			Instantiate(lobj,new Vector3(-4.2f,4.2f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 1){
+			Instantiate(lobj,new Vector3(-4.2f,1.8f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 2){
+			Instantiate(lobj,new Vector3(-4.2f,1.2f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 3){
+			Instantiate(lobj,new Vector3(-4.2f,0.6f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 4){
+			Instantiate(lobj,new Vector3(-4.2f,0f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 1){
+			Instantiate(lobj,new Vector3(-4.2f,-2.4f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 2){
+			Instantiate(lobj,new Vector3(-4.2f,-3.0f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 3){
+			Instantiate(lobj,new Vector3(-4.2f,-3.6f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 4){
+			Instantiate(lobj,new Vector3(-4.2f,-4.2f,0.0f),Quaternion.identity);
+			}
+			
+			if(ran == 1){
+			Instantiate(lobj,new Vector3(-4.2f,-1.8f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 2){
+			Instantiate(lobj,new Vector3(-4.2f,-1.2f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 3){
+			Instantiate(lobj,new Vector3(-4.2f,-0.6f,0.0f),Quaternion.identity);
+			}
+
 			return;
 		}
 		}
@@ -73,6 +158,112 @@ Vector3 prevPos;
 		
 		if (Input.GetKey (KeyCode.LeftArrow)) {
 			target = transform.position - MOVEX;
+			int ran = UnityEngine.Random.Range(0,5);
+
+			if(ran == 0){
+			Instantiate(obj,new Vector3(-1.2f,4.2f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 1){
+			Instantiate(obj,new Vector3(-0.6f,4.2f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 2){
+			Instantiate(obj,new Vector3(0.0f,4.2f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 3){
+			Instantiate(obj,new Vector3(0.6f,4.2f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 4){
+			Instantiate(obj,new Vector3(1.2f,4.2f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 0){
+			Instantiate(obj,new Vector3(-1.8f,4.2f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 1){
+			Instantiate(obj,new Vector3(-2.4f,4.2f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 2){
+			Instantiate(obj,new Vector3(-3.0f,4.2f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 3){
+			Instantiate(obj,new Vector3(1.8f,4.2f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 4){
+			Instantiate(obj,new Vector3(2.4f,4.2f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 0){
+			Instantiate(lobj,new Vector3(-4.2f,1.8f,0.0f),Quaternion.identity);
+			}
+
+
+			if(ran == 1){
+			Instantiate(lobj,new Vector3(-4.2f,2.4f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 2){
+			Instantiate(lobj,new Vector3(-4.2f,3.0f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 3){
+			Instantiate(lobj,new Vector3(-4.2f,3.6f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 4){
+			Instantiate(lobj,new Vector3(-4.2f,4.2f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 1){
+			Instantiate(lobj,new Vector3(-4.2f,1.8f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 2){
+			Instantiate(lobj,new Vector3(-4.2f,1.2f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 3){
+			Instantiate(lobj,new Vector3(-4.2f,0.6f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 4){
+			Instantiate(lobj,new Vector3(-4.2f,0f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 1){
+			Instantiate(lobj,new Vector3(-4.2f,-2.4f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 2){
+			Instantiate(lobj,new Vector3(-4.2f,-3.0f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 3){
+			Instantiate(lobj,new Vector3(-4.2f,-3.6f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 4){
+			Instantiate(lobj,new Vector3(-4.2f,-4.2f,0.0f),Quaternion.identity);
+			}
+			
+			if(ran == 1){
+			Instantiate(lobj,new Vector3(-4.2f,-1.8f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 2){
+			Instantiate(lobj,new Vector3(-4.2f,-1.2f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 3){
+			Instantiate(lobj,new Vector3(-4.2f,-0.6f,0.0f),Quaternion.identity);
+			}
 			return;
 		}
 		}
@@ -80,6 +271,112 @@ Vector3 prevPos;
 		if(this.transform.position.y != 4.2f){
 		if (Input.GetKey (KeyCode.UpArrow)) {
 			target = transform.position + MOVEY;
+			int ran = UnityEngine.Random.Range(0,5);
+
+			if(ran == 0){
+			Instantiate(obj,new Vector3(-1.2f,4.2f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 1){
+			Instantiate(obj,new Vector3(-0.6f,4.2f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 2){
+			Instantiate(obj,new Vector3(0.0f,4.2f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 3){
+			Instantiate(obj,new Vector3(0.6f,4.2f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 4){
+			Instantiate(obj,new Vector3(1.2f,4.2f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 0){
+			Instantiate(obj,new Vector3(-1.8f,4.2f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 1){
+			Instantiate(obj,new Vector3(-2.4f,4.2f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 2){
+			Instantiate(obj,new Vector3(-3.0f,4.2f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 3){
+			Instantiate(obj,new Vector3(1.8f,4.2f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 4){
+			Instantiate(obj,new Vector3(2.4f,4.2f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 0){
+			Instantiate(lobj,new Vector3(-4.2f,1.8f,0.0f),Quaternion.identity);
+			}
+
+
+			if(ran == 1){
+			Instantiate(lobj,new Vector3(-4.2f,2.4f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 2){
+			Instantiate(lobj,new Vector3(-4.2f,3.0f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 3){
+			Instantiate(lobj,new Vector3(-4.2f,3.6f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 4){
+			Instantiate(lobj,new Vector3(-4.2f,4.2f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 1){
+			Instantiate(lobj,new Vector3(-4.2f,1.8f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 2){
+			Instantiate(lobj,new Vector3(-4.2f,1.2f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 3){
+			Instantiate(lobj,new Vector3(-4.2f,0.6f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 4){
+			Instantiate(lobj,new Vector3(-4.2f,0f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 1){
+			Instantiate(lobj,new Vector3(-4.2f,-2.4f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 2){
+			Instantiate(lobj,new Vector3(-4.2f,-3.0f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 3){
+			Instantiate(lobj,new Vector3(-4.2f,-3.6f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 4){
+			Instantiate(lobj,new Vector3(-4.2f,-4.2f,0.0f),Quaternion.identity);
+			}
+			
+			if(ran == 1){
+			Instantiate(lobj,new Vector3(-4.2f,-1.8f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 2){
+			Instantiate(lobj,new Vector3(-4.2f,-1.2f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 3){
+			Instantiate(lobj,new Vector3(-4.2f,-0.6f,0.0f),Quaternion.identity);
+			}
 			return;
 		}
 		}
@@ -87,6 +384,112 @@ Vector3 prevPos;
 		if(this.transform.position.y != -4.2f){
 		if (Input.GetKey (KeyCode.DownArrow)) {
 			target = transform.position - MOVEY;
+			int ran = UnityEngine.Random.Range(0,5);
+
+			if(ran == 0){
+			Instantiate(obj,new Vector3(-1.2f,4.2f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 1){
+			Instantiate(obj,new Vector3(-0.6f,4.2f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 2){
+			Instantiate(obj,new Vector3(0.0f,4.2f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 3){
+			Instantiate(obj,new Vector3(0.6f,4.2f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 4){
+			Instantiate(obj,new Vector3(1.2f,4.2f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 0){
+			Instantiate(obj,new Vector3(-1.8f,4.2f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 1){
+			Instantiate(obj,new Vector3(-2.4f,4.2f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 2){
+			Instantiate(obj,new Vector3(-3.0f,4.2f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 3){
+			Instantiate(obj,new Vector3(1.8f,4.2f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 4){
+			Instantiate(obj,new Vector3(2.4f,4.2f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 0){
+			Instantiate(lobj,new Vector3(-4.2f,1.8f,0.0f),Quaternion.identity);
+			}
+
+
+			if(ran == 1){
+			Instantiate(lobj,new Vector3(-4.2f,2.4f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 2){
+			Instantiate(lobj,new Vector3(-4.2f,3.0f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 3){
+			Instantiate(lobj,new Vector3(-4.2f,3.6f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 4){
+			Instantiate(lobj,new Vector3(-4.2f,4.2f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 1){
+			Instantiate(lobj,new Vector3(-4.2f,1.8f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 2){
+			Instantiate(lobj,new Vector3(-4.2f,1.2f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 3){
+			Instantiate(lobj,new Vector3(-4.2f,0.6f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 4){
+			Instantiate(lobj,new Vector3(-4.2f,0f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 1){
+			Instantiate(lobj,new Vector3(-4.2f,-2.4f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 2){
+			Instantiate(lobj,new Vector3(-4.2f,-3.0f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 3){
+			Instantiate(lobj,new Vector3(-4.2f,-3.6f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 4){
+			Instantiate(lobj,new Vector3(-4.2f,-4.2f,0.0f),Quaternion.identity);
+			}
+			
+			if(ran == 1){
+			Instantiate(lobj,new Vector3(-4.2f,-1.8f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 2){
+			Instantiate(lobj,new Vector3(-4.2f,-1.2f,0.0f),Quaternion.identity);
+			}
+
+			if(ran == 3){
+			Instantiate(lobj,new Vector3(-4.2f,-0.6f,0.0f),Quaternion.identity);
+			}
 			return;
 		}
 		}
